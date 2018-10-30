dc35ddbdda2fab2624cc200b068a03a033cf496c
juanm <jbaader@itba.edu.ar>
2018-10-30 20:26:43 -0300

    Merge branch 'master' of https://github.com/FedeBrg/EDAReversi
    # Conflicts:
    #	src/back/Game.java

--- src/MinMax/MinMaxAI.java
+++ src/MinMax/MinMaxAI.java
@@ -1,4 +1,36 @@
 package MinMax;
 
+import back.Board;
+import back.Game;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 public class MinMaxAI {
+    Game game;
+    int color;
+
+    MinMaxAI(Game game,int color){
+        this.game=game;
+        this.color=color;
+    }
+
+    void MakeMove(){
+        List<Board> moves=this.getMoves();
+        Random rand=new Random();
+        int n=rand.nextInt();
+        game.board=moves.get(n%moves.size());
+    }
+    List<Board> getMoves(){
+        List<Board> moves= new LinkedList<>();
+        for(int i = 0;i<game.board.size; i++){
+            for (int j = 0; j < game.board.size; j++) {
+                Board aux =game.board.isValidMove(i,j,color);
+                if(aux!=null)
+                    moves.add(aux);
+            }
+        }
+
+    }
 }
--- src/MinMax/MinMaxNode.java
+++ src/MinMax/MinMaxNode.java
@@ -1,4 +1,13 @@
 package MinMax;
 
+import back.Board;
+
 public class MinMaxNode {
+    Board board;
+    int score;
+
+    MinMaxNode(Board board){
+        this.board=board;
+        this.score=board.score();
+    }
 }
