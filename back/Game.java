58da4d2c218fcf6e123fa4271d0394b72ad8508d
manolisho <37914259+manolisho@users.noreply.github.com>
2018-10-30 20:17:22 -0300

    Add files via upload
    Agrego los metodos del undo, la clase UndoNode y el undoStack

--- back/Game.java
+++ back/Game.java
@@ -1,17 +1,49 @@
 package back;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Game {
 	private Player p1;
 	private Player p2;
 	
 	private Board board;
-	
+	private Deque<UndoNode> undoStack;
 	private Player current;
-	
 	private boolean podas;
-	
 	private int gameMode;
 	
+	public class UndoNode {
+		Board board;
+		Player current;
+		
+		UndoNode(Board board, Player current){
+			this.board = board;
+			this.current = current;
+		}
+	}
 	
+	Game(Player p1, Player p2, Player current, boolean podas, int gameMode){
+		this.board = new Board();
+		this.undoStack = new LinkedList<UndoNode>();
+		this.p1 = p1;
+		this.p2 = p2;
+		this.current = current;
+		this.podas = podas;
+		this.gameMode = gameMode;
+	}
 	
+	public void undo() {
+		if(undoStack.isEmpty()) {
+			return;
+		}
+		
+		UndoNode toReplace = undoStack.pop();
+		board = toReplace.board;
+		current = toReplace.current;
+	}
+	
+	public void pushToStack() {
+		undoStack.push(new UndoNode(board, current));
+	}
 }
