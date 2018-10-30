8aa6405e1f856121b78fedd2a4cb4aab851e0690
FedeBrg <37914230+FedeBrg@users.noreply.github.com>
2018-10-29 23:19:42 -0300

    Add files via upload

---
+++ back/Board.java
@@ -0,0 +1,21 @@
+package back;
+
+import java.util.List;
+
+public class Board {
+	
+	private Piece[][] board;
+	private int score;
+	private int size;
+	
+	public List<Board> possibleMoves(){
+		return null;
+		
+	}
+	
+	private boolean isValidMove(Board move) {
+		return false;
+		
+	}
+
+}
---
+++ back/Game.java
@@ -0,0 +1,17 @@
+package back;
+
+public class Game {
+	private Player p1;
+	private Player p2;
+	
+	private Board board;
+	
+	private Player current;
+	
+	private boolean podas;
+	
+	private int gameMode;
+	
+	
+	
+}
