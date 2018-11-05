641dfad640b02bdc0ea6c8a6a88bc84437392397
manolisho <37914259+manolisho@users.noreply.github.com>
2018-11-05 09:04:18 -0300

    Add files via upload

--- src/MinMax/MinMaxAI.java
+++ src/MinMax/MinMaxAI.java
@@ -3,15 +3,18 @@ package MinMax;
 import back.Board;
 import back.Game;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class MinMaxAI {
-    private int lastScore;
-    private int color;
-    private int nodeNumber;
+    public int color;
+    int nodeNumber;
+    int lastScore;
 
     public MinMaxAI(int color){
         this.color=color;
@@ -26,32 +29,30 @@ public class MinMaxAI {
 
     public int[][] minMax(List<int[][]> moves,int depth, Game game){
         Board board;
-        int score=0,auxScore;
-        board=new  Board(game.board.getSize()); // CAMBIAR
-        Boolean myTurn=true;
-        Game current=game;
+        int score = 0, auxScore;
+        board = new  Board(game.board.getSize());
+        board.setBoard(moves.get(0));
+        Boolean myTurn = true;
+        Game current = new Game(game.board.getSize(), game.getWhoStart(), game.getAiType(), game.getLimit(), (game.getPodas())?"on":"off");
         current.switchPlayer();
-        boolean hasValue=false;
+        boolean hasValue = false;
         StringBuilder DOT= new StringBuilder("graph ARBOL{\n"); //inicializo el DOT
         this.nodeNumber=0;
         int currentNodeNumber=0;
-        Integer poda=null;
         for(int[][] move: moves){
             DOT.append(currentNodeNumber).append("--"); //se conecta a
 
             nodeNumber+=1;
 
-            auxScore=minMaxRec(move,depth-1, !myTurn, current,DOT,poda);
+            auxScore=minMaxRec(move,depth-1, !myTurn, current,DOT,null);
             if(!hasValue){
                 board.setBoard(move);
                 score=auxScore;
                 hasValue=true;
-                poda=score;
             }
             else if(auxScore>score){
                 board.setBoard(move);
                 score=auxScore;
-                poda=score;
             }
 
         }
@@ -62,7 +63,6 @@ public class MinMaxAI {
         StringSelection selection = new StringSelection(DOT.toString());
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(selection, selection);
-        System.out.println(score);
 
         return board.getBoard();
     }
@@ -93,47 +93,23 @@ public class MinMaxAI {
             if(!hasValue){
                 hasValue=true;
                 score=auxScore;
-                podaLocal=score;
-                if(poda!=null) {
-                    if (!myTurn && score<=poda ) {
-                        DOT.append(currentNodeNumber).append(" ").append("[label=\"").append(auxScore).append("\"]\n");
-                        return score;
-                    }
-                    if (myTurn && score>= poda) {
-                        DOT.append(currentNodeNumber).append(" ").append("[label=\"").append(auxScore).append("\"]\n");
-                        return score;
-                    }
-                }
+                if(poda!=null)
+                    podaLocal=score;
             }
             else {
                 if (myTurn && auxScore > score) {
                     score=auxScore;
                     podaLocal=score;
-                    if(poda!=null){
-                        if(score>=poda){
-                            DOT.append(currentNodeNumber).append(" ").append("[label=\"").append(auxScore).append("\"]\n");
-                            return score;
-                        }
-                    }
                 } else if (!myTurn && auxScore < score) {
                     score= auxScore;
                     podaLocal=score;
-                    if(poda!=null){
-                        if(score<=poda){
-                            DOT.append(currentNodeNumber).append(" ").append("[label=\"").append(auxScore).append("\"]\n");
-                            return score;
-                        }
-                    }
-
                 }
             }
         }
+        
         DOT.append(currentNodeNumber).append(" ").append("[label=\"").append(score).append("\"]\n");
         lastScore=game.board.score;
         return score;
     }
 
-    public int getColor() {
-        return color;
-    }
 }
